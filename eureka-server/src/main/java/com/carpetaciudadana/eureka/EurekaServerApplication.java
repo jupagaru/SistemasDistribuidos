4c5ce720373bebdb31e72a974db0d1eaeb16bc47
Juan Garcia <jupagaru@gmail.com>
2022-03-22 21:53:06 -0500

    Se genera jar gateway

--- eureka-server/src/main/java/com/carpetaciudadana/eureka/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/carpetaciudadana/eureka/EurekaServerApplication.java
@@ -1,11 +1,11 @@
 package com.carpetaciudadana.eureka;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
-@SpringBootApplication
+@EnableAutoConfiguration
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
