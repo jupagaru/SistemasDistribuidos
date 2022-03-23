d8d8872fdeb685b59a3cb3ec3ff143e9442767c0
Juan Garcia <jupagaru@gmail.com>
2022-03-23 00:29:41 -0500

    Se generan nuevos jar

--- eureka-server/src/main/java/com/carpetaciudadana/eureka/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/carpetaciudadana/eureka/EurekaServerApplication.java
@@ -1,11 +1,11 @@
 package com.carpetaciudadana.eureka;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
-@EnableAutoConfiguration
+@SpringBootApplication
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
