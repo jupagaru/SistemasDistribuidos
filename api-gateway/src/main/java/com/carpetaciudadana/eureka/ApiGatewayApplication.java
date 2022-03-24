543e5d04d240498fe6eefa81fcdce9af444fcd97
Juan Garcia <jupagaru@gmail.com>
2022-03-23 20:21:16 -0500

    Se modifica jar y gateway

--- api-gateway/src/main/java/com/carpetaciudadana/eureka/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/carpetaciudadana/eureka/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.carpetaciudadana.apigateway;
+package com.carpetaciudadana.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
