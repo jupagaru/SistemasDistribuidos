afbe6ba094aa0ad2e1b056777c02def1af815705
Juan Garcia <jupagaru@gmail.com>
2022-03-26 01:11:53 -0500

    Jar

--- carpeta-ciudadana-authentication/src/main/java/com/carpetaciudadana/authentication/config/KeycloakConfig.java
+++ carpeta-ciudadana-authentication/src/main/java/com/carpetaciudadana/authentication/config/KeycloakConfig.java
@@ -12,8 +12,8 @@ public class KeycloakConfig {
     final static String serverUrl = "http://localhost:8080/auth";
     public final static String realm = "master";
     final static String clientId = "carpeta-ciudadana";
-    final static String clientSecret = "66b4d569-2ac6-4a35-9f91-3a7d27fd0380";
-    //final static String clientSecret = "3e32405d-e9f2-46a4-a449-e53df4325a2c";
+    //final static String clientSecret = "66b4d569-2ac6-4a35-9f91-3a7d27fd0380";
+    final static String clientSecret = "3e32405d-e9f2-46a4-a449-e53df4325a2c";
     final static String userName = "admin";
     final static String password = "admin";
 
